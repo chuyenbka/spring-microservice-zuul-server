4008b447a3b0d733a7e0d6b5cbc36085deed771d
chuyenpn <chuyenpn@rikkeisoft.com>
2020-07-09 17:13:41 +0700

    add annotation enableZuulProxy

--- src/main/java/com/chuyenpn/zuulserver/ZuulServerApplication.java
+++ src/main/java/com/chuyenpn/zuulserver/ZuulServerApplication.java
@@ -3,9 +3,10 @@ package com.chuyenpn.zuulserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
-@EnableZuulServer
+@EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ZuulServerApplication {
